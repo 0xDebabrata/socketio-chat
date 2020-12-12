7111bce0ebb656e97766a5028ae55df39fa111a8
Debabrata Mondal <debabrata.js@protonmail.com>
2020-12-12 11:50:49 +0530

    notify when new user joins

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ let io = require('socket.io')(http);
 app.use(express.static('public'))
 
 io.on('connection', (socket) => {
+
+  // Broadcast when user joins chat
+  socket.on('connect', () => {
+    msg = 'User has entered the chat'
+    socket.broadcast.emit('welcome', msg)
+  })
+
+  // Broadcast when a user disconnects
   socket.on('disconnect', () => {
     msg = socket.id + ' has disconnected'
     socket.broadcast.emit('user::disconnect', msg)
   })
   
+  // Broadcast user messages
   socket.on('chat message', (msg) => {
     socket.broadcast.emit('chat message', msg)
   })
+
 })
 
 http.listen(3000, () => {
--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ let socket = io('ws://localhost:3000');
 let message = document.getElementById('input')
 let list = document.getElementById('messages')
 
+// Send chat message to server for broadcast
 let emit = event => {
   event.preventDefault()
   socket.emit('chat message', message.value);
@@ -9,12 +10,22 @@ let emit = event => {
   return false
 }
 
+// Display chat messages
 socket.on('chat message', (msg) => {
   let li = document.createElement('li')
-  li.appendChild(document.createTextNode(msg))
+  li.innerHTML = msg
   list.appendChild(li)
 })
 
+// User disconnect alert
 socket.on('user::disconnect', msg => {
   alert(msg)
+})
+
+// Welcome message when new user joins chat
+socket.on('welcome', msg => {
+  let li = document.createElement('li')
+  li.setAttribute('class', 'welcome')
+  li.innerHTML = msg
+  list.appendChild(li)
 })
